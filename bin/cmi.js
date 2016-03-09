f216084035d0eb9c2c7fef1e08edd0df716d8248
minodisk <daisuke.mino@gmail.com>
2016-03-09 19:50:52 +0900

    Use promise

--- bin/cmi.js
+++ bin/cmi.js
@@ -2,7 +2,7 @@
 
 const cli = require('../lib/cli')
 const argv = cli.parse(process.argv)
-const inject = require('../lib/inject')
+const inject = require('../lib/inject').injectWithOptions
 
 inject(argv)
   .catch((err) => {
